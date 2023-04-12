3e54c80e421bb0502da7df022cf7f54059e0ab35
akshay chopade <akshay.chopade@pattern.com>
2023-04-12 12:52:11 +0530

    Added delete and destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -35,9 +35,13 @@ class TasksController < ApplicationController
   end
 
   def delete
+    @task = Task.find(params[:id])
   end
-  
+
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
   def task_params
